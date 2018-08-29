d309becc2be7871ff8bd57be4cb20167c2ad5c7a
Yifei Zhang <t-yifzha@microsoft.com>
2018-08-29 22:10:07 +0000

    Merged PR 1052845: update configbuilder data struct
    update configbuilder data struct

---
+++ pkg/appgw/configbuilder.go
@@ -0,0 +1,104 @@
+package appgw
+
+import (
+	"github.com/Azure/Networking-AppGW-k8s/pkg/k8scontext"
+	"github.com/Azure/Networking-AppGW-k8s/pkg/utils"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-06-01/network"
+
+	"k8s.io/api/extensions/v1beta1"
+)
+
+// AppGwConfigBuilder is a builder for application gateway configuration
+type AppGwConfigBuilder interface {
+	// builder pattern
+	BackendHTTPSettingsCollection(ingressList [](*v1beta1.Ingress)) (AppGwConfigBuilder, error)
+	BackendAddressPools(ingressList [](*v1beta1.Ingress)) (AppGwConfigBuilder, error)
+	HTTPListeners(ingressList [](*v1beta1.Ingress)) (AppGwConfigBuilder, error)
+	RequestRoutingRules(ingressList [](*v1beta1.Ingress)) (AppGwConfigBuilder, error)
+
+	Build() *network.ApplicationGatewayPropertiesFormat
+}
+
+type appGwConfigBuilder struct {
+	serviceBackendPairMap map[backendIdentifier](serviceBackendPortPair)
+
+	ingressKeyHostnameSecretIDMap map[string](map[string]secretIdentifier)
+	secretIDCertificateMap        map[secretIdentifier]*string
+
+	httpListenersMap            map[frontendListenerIdentifier](*network.ApplicationGatewayHTTPListener)
+	httpListenersAzureConfigMap map[frontendListenerIdentifier](*frontendListenerAzureConfig)
+
+	backendHTTPSettingsMap map[backendIdentifier](*network.ApplicationGatewayBackendHTTPSettings)
+
+	backendPoolMap map[backendIdentifier](*network.ApplicationGatewayBackendAddressPool)
+
+	k8sContext      *k8scontext.Context
+	appGwIdentifier Identifier
+	appGwConfig     network.ApplicationGatewayPropertiesFormat
+}
+
+// NewAppGwConfigBuilder construct a builder
+func NewAppGwConfigBuilder(context *k8scontext.Context, appGwIdentifier *Identifier, originalConfig *network.ApplicationGatewayPropertiesFormat) AppGwConfigBuilder {
+	return &appGwConfigBuilder{
+		serviceBackendPairMap:         make(map[backendIdentifier](serviceBackendPortPair)),
+		httpListenersMap:              make(map[frontendListenerIdentifier](*network.ApplicationGatewayHTTPListener)),
+		httpListenersAzureConfigMap:   make(map[frontendListenerIdentifier](*frontendListenerAzureConfig)),
+		ingressKeyHostnameSecretIDMap: make(map[string](map[string]secretIdentifier)),
+		secretIDCertificateMap:        make(map[secretIdentifier]*string),
+		backendHTTPSettingsMap:        make(map[backendIdentifier](*network.ApplicationGatewayBackendHTTPSettings)),
+		backendPoolMap:                make(map[backendIdentifier](*network.ApplicationGatewayBackendAddressPool)),
+		k8sContext:                    context,
+		appGwIdentifier:               *appGwIdentifier,
+		appGwConfig:                   *originalConfig,
+	}
+}
+
+// resolvePortName function goes through the endpoints of a given service and
+// look for possible port number corresponding to a port name
+func (builder *appGwConfigBuilder) resolvePortName(portName string, backendID *backendIdentifier) utils.UnorderedSet {
+	endpoints := builder.k8sContext.GetEndpointsByService(backendID.serviceKey())
+	resolvedPorts := utils.NewUnorderedSet()
+	for _, subset := range endpoints.Subsets {
+		for _, epPort := range subset.Ports {
+			if epPort.Name == portName {
+				resolvedPorts.Insert(epPort.Port)
+			}
+		}
+	}
+	return resolvedPorts
+}
+
+func generateBackendID(ingress *v1beta1.Ingress, backend *v1beta1.IngressBackend) backendIdentifier {
+	backendServiceName := backend.ServiceName
+	backendServicePort := backend.ServicePort
+	backendID := backendIdentifier{
+		serviceIdentifier: serviceIdentifier{
+			Namespace: ingress.Namespace,
+			Name:      backendServiceName,
+		},
+		ServicePort: backendServicePort,
+	}
+	return backendID
+}
+
+func generateFrontendListenerID(rule *v1beta1.IngressRule,
+	protocol network.ApplicationGatewayProtocol, overridePort *int32) frontendListenerIdentifier {
+	frontendPort := int32(80)
+	if protocol == network.HTTPS {
+		frontendPort = int32(443)
+	}
+	if overridePort != nil {
+		frontendPort = *overridePort
+	}
+	frontendListenerID := frontendListenerIdentifier{
+		FrontendPort: frontendPort,
+		HostName:     rule.Host,
+	}
+	return frontendListenerID
+}
+
+// Build generates the ApplicationGatewayPropertiesFormat for azure resource manager
+func (builder *appGwConfigBuilder) Build() *network.ApplicationGatewayPropertiesFormat {
+	config := builder.appGwConfig
+	return &config
+}
---
+++ pkg/appgw/internaltypes.go
@@ -0,0 +1,113 @@
+package appgw
+
+import (
+	"fmt"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-06-01/network"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+type backendIdentifier struct {
+	serviceIdentifier
+	ServicePort intstr.IntOrString
+}
+
+type serviceBackendPortPair struct {
+	ServicePort int32
+	BackendPort int32
+}
+
+type frontendListenerIdentifier struct {
+	FrontendPort int32
+	HostName     string
+}
+
+type serviceIdentifier struct {
+	Namespace string
+	Name      string
+}
+
+type secretIdentifier struct {
+	Namespace string
+	Name      string
+}
+
+// create xxx -> xxxconfiguration mappings to contain all the information
+type frontendListenerAzureConfig struct {
+	Protocol network.ApplicationGatewayProtocol
+	Secret   secretIdentifier
+}
+
+func (s serviceIdentifier) serviceFullName() string {
+	return fmt.Sprintf("%v-%v", s.Namespace, s.Name)
+}
+
+func (s serviceIdentifier) serviceKey() string {
+	return fmt.Sprintf("%v/%v", s.Namespace, s.Name)
+}
+
+func (s secretIdentifier) secretKey() string {
+	return fmt.Sprintf("%v/%v", s.Namespace, s.Name)
+}
+
+func (s secretIdentifier) secretFullName() string {
+	return fmt.Sprintf("%v-%v", s.Namespace, s.Name)
+}
+
+func getResourceKey(namespace, name string) string {
+	return fmt.Sprintf("%v/%v", namespace, name)
+}
+
+func generateHTTPSettingsName(serviceName string, servicePort string, backendPortNo int32) string {
+	return fmt.Sprintf("k8s-%v-%v-bp-%v", serviceName, servicePort, backendPortNo)
+}
+
+func generateAddressPoolName(serviceName string, servicePort string, backendPortNo int32) string {
+	return fmt.Sprintf("k8s-%v-%v-bp-%v-pool", serviceName, servicePort, backendPortNo)
+}
+
+func generateFrontendPortName(port int32) string {
+	return fmt.Sprintf("k8s-fp-%v", port)
+}
+
+func generateHTTPListenerName(frontendListenerID frontendListenerIdentifier) string {
+	return fmt.Sprintf("k8s-%v-%v-fl", frontendListenerID.HostName, frontendListenerID.FrontendPort)
+}
+
+func generateURLPathMapName(frontendListenerID frontendListenerIdentifier) string {
+	return fmt.Sprintf("k8s-%v-%v-url", frontendListenerID.HostName, frontendListenerID.FrontendPort)
+}
+
+func generateRequestRoutingRuleName(frontendListenerID frontendListenerIdentifier) string {
+	return fmt.Sprintf("k8s-%v-%v-rr", frontendListenerID.HostName, frontendListenerID.FrontendPort)
+}
+
+const defaultBackendHTTPSettingsName = "k8s-defaulthttpsetting"
+const defaultBackendAddressPoolName = "k8s-defaultaddresspool"
+
+func defaultBackendHTTPSettings() network.ApplicationGatewayBackendHTTPSettings {
+	defHTTPSettingsName := defaultBackendHTTPSettingsName
+	defHTTPSettingsPort := int32(80)
+	return network.ApplicationGatewayBackendHTTPSettings{
+		Name: &defHTTPSettingsName,
+		ApplicationGatewayBackendHTTPSettingsPropertiesFormat: &network.ApplicationGatewayBackendHTTPSettingsPropertiesFormat{
+			Protocol: network.HTTP,
+			Port:     &defHTTPSettingsPort,
+		},
+	}
+}
+
+func defaultBackendAddressPool() network.ApplicationGatewayBackendAddressPool {
+	defBackendAddressPool := defaultBackendAddressPoolName
+	return network.ApplicationGatewayBackendAddressPool{
+		Name: &defBackendAddressPool,
+		ApplicationGatewayBackendAddressPoolPropertiesFormat: &network.ApplicationGatewayBackendAddressPoolPropertiesFormat{},
+	}
+}
+
+func defaultFrontendListenerIdentifier() frontendListenerIdentifier {
+	return frontendListenerIdentifier{
+		FrontendPort: int32(80),
+		HostName:     "",
+	}
+}
