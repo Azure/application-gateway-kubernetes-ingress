667834b0cfed18027f9836e589d2b7181c763e13
Yifei Zhang <t-yifzha@microsoft.com>
2018-08-29 22:10:51 +0000

    Merged PR 1052850: implement backend address pool and settings config generator
    implement backend address pool and settings config generator

---
+++ pkg/appgw/backendaddresspools.go
@@ -0,0 +1,63 @@
+package appgw
+
+import (
+	"github.com/Azure/Networking-AppGW-k8s/pkg/utils"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-06-01/network"
+	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/golang/glog"
+	"k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
+)
+
+func (builder *appGwConfigBuilder) BackendAddressPools(ingressList [](*v1beta1.Ingress)) (AppGwConfigBuilder, error) {
+	addressPools := make([](network.ApplicationGatewayBackendAddressPool), 0)
+	addressPools = append(addressPools, defaultBackendAddressPool())
+
+	for backendID, serviceBackendPair := range builder.serviceBackendPairMap {
+		endpoints := builder.k8sContext.GetEndpointsByService(backendID.serviceKey())
+		if endpoints == nil {
+			glog.Warningf("unable to get endpoints for service key [%s]", backendID.serviceKey())
+			return builder, error.Error("unable to get endpoints for service")
+		}
+		for _, subset := range endpoints.Subsets {
+			endpointsPortsSet := utils.NewUnorderedSet()
+			for _, endpointsPort := range subset.Ports {
+				if endpointsPort.Protocol != v1.ProtocolTCP {
+					continue
+				}
+				endpointsPortsSet.Insert(endpointsPort.Port)
+			}
+
+			if endpointsPortsSet.Contains(serviceBackendPair.BackendPort) {
+				addressPoolName := generateAddressPoolName(backendID.serviceFullName(), backendID.ServicePort.String(), serviceBackendPair.BackendPort)
+				addressPoolAddresses := make([](network.ApplicationGatewayBackendAddress), 0)
+				for _, address := range subset.Addresses {
+					ip := address.IP
+					hostname := address.Hostname
+					// prefer IP address
+					if len(ip) != 0 {
+						// address specified by ip
+						addressPoolAddresses = append(addressPoolAddresses, network.ApplicationGatewayBackendAddress{IPAddress: &ip})
+					} else if len(address.Hostname) != 0 {
+						// address specified by hostname
+						addressPoolAddresses = append(addressPoolAddresses, network.ApplicationGatewayBackendAddress{Fqdn: &hostname})
+					}
+				}
+				addressPool := network.ApplicationGatewayBackendAddressPool{
+					Etag: to.StringPtr("*"),
+					Name: &addressPoolName,
+					ApplicationGatewayBackendAddressPoolPropertiesFormat: &network.ApplicationGatewayBackendAddressPoolPropertiesFormat{
+						BackendAddresses: &addressPoolAddresses,
+					},
+				}
+				addressPools = append(addressPools, addressPool)
+				builder.backendPoolMap[backendID] = &addressPool
+				break
+			}
+		}
+	}
+
+	builder.appGwConfig.BackendAddressPools = &addressPools
+
+	return builder, nil
+}
