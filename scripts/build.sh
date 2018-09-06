7683c49bd0650d608267b3d3586f05a751753031
Yifei Zhang <t-yifzha@microsoft.com>
2018-09-06 12:28:07 -0700

    minor changes to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-/bin/echo -e "\e[44;97m Compiling ... \e[0m"
+echo -e "\e[44;97m Compiling ... \e[0m"
 if GOOS=linux GOBIN=`pwd`/bin go install -v ./cmd/appgw-ingress; then
     chmod -R 777 bin
-    /bin/echo -e "\e[42;97m SUCCESS \e[0m"
+    echo -e "\e[42;97m SUCCESS \e[0m"
 else
-    /bin/echo -e "\e[101;97m FAILED \e[0m"
+    echo -e "\e[101;97m FAILED \e[0m"
     exit 1
 fi
