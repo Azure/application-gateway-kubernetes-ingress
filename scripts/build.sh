077d9438fa10fc5dd54daf3b43ed6cdfbe95a54f
Yifei Zhang <njzhangyifei@gmail.com>
2018-09-06 15:43:17 -0700

    Yifei/travis-fix set up travis-ci (#5)
    * resolve devenv&deploy docker image naming conflict
    
    * fix permission issues
    
    * fix .travis.yml
    
    * fix redundant dependency
    
    * minor changes to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
-/bin/echo -e "\e[44;97m Compiling ... \e[0m"
+#!/bin/bash
+echo -e "\e[44;97m Compiling ... \e[0m"
 if GOOS=linux GOBIN=`pwd`/bin go install -v ./cmd/appgw-ingress; then
-    /bin/echo -e "\e[42;97m SUCCESS \e[0m"
+    chmod -R 777 bin
+    echo -e "\e[42;97m SUCCESS \e[0m"
 else
-    /bin/echo -e "\e[101;97m FAILED \e[0m"
+    echo -e "\e[101;97m FAILED \e[0m"
     exit 1
 fi
