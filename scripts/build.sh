a2b189ec82bfb8a8ee04ee8ee724fbbf71b35988
Vijayan Ramakrishnan <vijayan.ramakrishnan@gmail.com>
2018-09-24 15:05:51 -0700

    Address code review comments #1
    - Moved lint and vet checks before build
    - golint always return 0 status. Hence redirected output to file
      to check for any failures.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
+
+export GOOS=linux
+export GOBIN=`pwd`/bin
+GO_PKGS="./cmd/... ./pkg/..."
+GO_PROJ="github.com/Azure/application-gateway-kubernetes-ingress"
+
+echo -e "\e[44;97m Running go lint.. \e[0m"
+golint $GO_PKGS > ./lint.out
+cat ./lint.out 
+if [ -s ./lint.out ]; then
+    echo -e "\e[101;97m GOLINT FAILED \e[0m"``
+    exit 1
+else
+    echo -e "\e[42;97m GOLINT SUCCESS \e[0m"
+fi
+
+echo -e "\e[44;97m Running go vet ... \e[0m"
+if go vet -v -source $GO_PKGS; then
+    echo -e "\e[42;97m GOVET SUCCESS \e[0m"
+else
+    echo -e "\e[101;97m GOVET FAILED \e[0m"``
+    exit 1
+fi
+
 echo -e "\e[44;97m Compiling ... \e[0m"
-if GOOS=linux GOBIN=`pwd`/bin go install -v ./cmd/appgw-ingress; then
+if  go install -v ./cmd/appgw-ingress; then
     chmod -R 777 bin
     echo -e "\e[42;97m BUILD SUCCESS \e[0m"
 else
     echo -e "\e[101;97m BUILD FAILED \e[0m"
-    exit 1
-fi
-
-echo -e "\e[44;97m Running go lint ... \e[0m"
-if GOOS=linux GOBIN=`pwd`/bin golint ./cmd/... ./pkg/...; then
-    chmod -R 777 bin
-    echo -e "\e[42;97m GOLINT SUCCESS \e[0m"
-else
-    echo -e "\e[101;97m GOLINT FAILED \e[0m"
-    exit 1
-fi
-
-echo -e "\e[44;97m Running go vet ... \e[0m"
-if GOOS=linux GOBIN=`pwd`/bin go vet -v -source ./cmd/... ./pkg/...; then
-    chmod -R 777 bin
-    echo -e "\e[42;97m GOVET SUCCESS \e[0m"
-else
-    echo -e "\e[101;97m GOVET FAILED \e[0m"
     exit 1
 fi
