50de23f2015ba4a4c3919e9f19257673f5f12d35
Vijayan Ramakrishnan <vijayan.ramakrishnan@gmail.com>
2018-09-25 11:43:31 -0700

    Fix build script to exclude govet on vendored pkgs

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,32 +2,33 @@
 
 export GOOS=linux
 export GOBIN=`pwd`/bin
-GO_PKGS="./cmd/... ./pkg/..."
 GO_PROJ="github.com/Azure/application-gateway-kubernetes-ingress"
+GO_PKGS=`go list ./... | grep -v vendor/`
+echo $GO_PKGS
 
 echo -e "\e[44;97m Running go lint.. \e[0m"
-golint $GO_PKGS > ./lint.out
-cat ./lint.out 
-if [ -s ./lint.out ]; then
-    echo -e "\e[101;97m GOLINT FAILED \e[0m"``
+golint $GO_PKGS > /tmp/lint.out
+cat /tmp/lint.out 
+if [ -s /tmp/lint.out ]; then
+    echo -e "\e[101;97m golint FAILED \e[0m"``
     exit 1
 else
-    echo -e "\e[42;97m GOLINT SUCCESS \e[0m"
+    echo -e "\e[42;97m golint SUCCESS \e[0m"
 fi
 
-echo -e "\e[44;97m Running go vet ... \e[0m"
-if go vet -v -source $GO_PKGS; then
-    echo -e "\e[42;97m GOVET SUCCESS \e[0m"
+echo -e "\e[44;97m Running govet ... \e[0m"
+if go vet -v $GO_PKGS; then
+    echo -e "\e[42;97m govet SUCCESS \e[0m"
 else
-    echo -e "\e[101;97m GOVET FAILED \e[0m"``
+    echo -e "\e[101;97m govet FAILED \e[0m"``
     exit 1
 fi
 
 echo -e "\e[44;97m Compiling ... \e[0m"
 if  go install -v ./cmd/appgw-ingress; then
     chmod -R 777 bin
-    echo -e "\e[42;97m BUILD SUCCESS \e[0m"
+    echo -e "\e[42;97m Build SUCCESS \e[0m"
 else
-    echo -e "\e[101;97m BUILD FAILED \e[0m"
+    echo -e "\e[101;97m Build FAILED \e[0m"
     exit 1
 fi
