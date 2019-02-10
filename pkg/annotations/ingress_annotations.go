81730eebac822096b2c7529a8cf576bcccc27f90
Avinash Sridharan <avinash.sridharan@microsoft.com>
2019-02-09 22:16:10 -0800

    Updated annotations package with all supported AG annotations.

--- pkg/annotations/ingress_annotations.go
+++ pkg/annotations/ingress_annotations.go
@@ -1,51 +1,37 @@
 package annotations
 
 import (
-	extensions "k8s.io/api/extensions/v1beta1"
+	"k8s.io/api/extensions/v1beta1"
 )
 
 const (
-	// Prefix AppGw Prefix
-	Prefix = "appgw.ingress.kubernetes.io"
-
-	// BackendPathPrefix defines the value for Path which should be used as a prefix for all HTTP requests.
+	// BackendPathPrefixKey defines the value for Path which should be used as a prefix for all HTTP requests.
 	// Null means no path will be prefixed. Default value is null.
-	BackendPathPrefix = "appgw.ingress.kubernetes.io/backend-path-prefix"
+	BackendPathPrefixKey = "appgw.ingress.kubernetes.io/backend-path-prefix"
 
-	// IngressClass defines the key of the annotation which needs to be set in order to specify
+	// IngressClassKey defines the key of the annotation which needs to be set in order to specify
 	// that this is an ingress resource meant for the application gateway ingress controller.
 	IngressClassKey = "kubernetes.io/ingress.class"
 
-	// IngressControllerName defines the value of the `IngressClass` annotation that will tell the ingress controller
+	// ApplicationGatewayIngressClass defines the value of the `IngressClassKey` annotation that will tell the ingress controller
 	// whether it should act on this ingress resource or not.
-	IngressControllerName = "azure/application-gateway"
+	ApplicationGatewayIngressClass = "azure/application-gateway"
 )
 
-// IngressAnnotations helper
-type IngressAnnotations struct {
-	annotations map[string]string
-}
-
-// FromIngress Annotations
-func FromIngress(ing *extensions.Ingress) *IngressAnnotations {
-	return &IngressAnnotations{ing.Annotations}
-}
-
-// OverrideBackendPath override path
-func (ing *IngressAnnotations) BackendPathPrefix() string {
-	val, ok := ing.annotations[BackendPathPrefix]
-	if !ok {
-		return ""
-	}
-
+// BackendPathPrefix override path
+func BackendPathPrefix(ing *v1beta1.Ingress) string {
+	val, _ := ing.Annotations[BackendPathPrefixKey]
 	return val
 }
 
 // IngressClass ingress class
-func (ing *IngressAnnotations) IngressClass() string {
-	val, ok := ing.annotations[IngressClassKey]
-	if !ok {
-		return ""
-	}
+func IngressClass(ing *v1beta1.Ingress) string {
+	val, _ := ing.Annotations[IngressClassKey]
 	return val
+}
+
+// IsApplicationGatewayIngress checks if the Ingress resource can be handled by the Application Gateway ingress controller.
+func IsApplicationGatewayIngress(ing *v1beta1.Ingress) bool {
+	controllerName := ing.Annotations[IngressClassKey]
+	return controllerName == ApplicationGatewayIngressClass
 }
