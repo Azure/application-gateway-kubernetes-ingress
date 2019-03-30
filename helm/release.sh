656ab122406120084043e114e06d13d9124a090c
Avinash Sridharan <avinash.sridharan@microsoft.com>
2019-03-30 14:29:56 -0700

    Updated the helm release to 0.3.0.

--- helm/release.sh
+++ helm/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo " - running helm package"
-helm package ingress-azure --version 0.2.0
+helm package ingress-azure --version 0.3.0
 echo " - updating helm repo index"
 helm repo index . --url https://azure.github.io/application-gateway-kubernetes-ingress/helm
 echo " - done!"
