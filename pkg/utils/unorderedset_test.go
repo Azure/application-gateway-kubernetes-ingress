130fbe40eb8f3b3416fd599b612ffb65e1707390
Delyan Raychev <delyan.raychev@microsoft.com>
2019-05-06 12:17:43 -0700

    unordered-set: Fix flaky test

--- pkg/utils/unorderedset_test.go
+++ pkg/utils/unorderedset_test.go
@@ -7,6 +7,7 @@ package utils
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
@@ -16,12 +17,17 @@ func TestToSlice(t *testing.T) {
 	set.Insert("two")
 	set.Insert("one")
 	expected := []string{"one", "two"}
-	actual := set.ToSlice()
+	actualSlice := set.ToSlice()
+	actual := []string{
+		actualSlice[0].(string),
+		actualSlice[1].(string),
+	}
+	sort.Strings(actual)
 	if len(actual) != 2 {
 		t.Error(fmt.Sprintf("Expected length to be 2; It is %d", len(actual)))
 	}
 
-	if actual[0] != expected[0] && actual[1] != expected[1] {
+	if actual[0] != expected[0] || actual[1] != expected[1] {
 		t.Error(fmt.Sprintf("\nExpected: %+v\nActually: %+v\n", expected, actual))
 	}
 }
