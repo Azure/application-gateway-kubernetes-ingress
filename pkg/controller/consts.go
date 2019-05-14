f228b2dee5cade07fc55b8d20bf9dd69a5965247
Delyan Raychev <delyan.raychev@microsoft.com>
2019-05-14 12:04:20 -0700

    s/is-managed-by-k8s-ingress/managed-by-k8s-ingress/g

--- pkg/controller/consts.go
+++ pkg/controller/consts.go
@@ -1,4 +1,4 @@
 package controller
 
 // An App Gateway tag: Resources tagged with this are exclusively managed by a Kubernetes Ingress.
-const isManagedByK8sIngress = "is-managed-by-k8s-ingress"
+const isManagedByK8sIngress = "managed-by-k8s-ingress"
