3ee77cb62c1000a4d1a8c9245120d197ee7264af
Delyan Raychev <delyan.raychev@microsoft.com>
2019-05-17 15:14:53 -0700

    Better organization; Comments

--- pkg/controller/helpers.go
+++ pkg/controller/helpers.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"reflect"
 	"strings"
 
@@ -33,25 +32,40 @@ func configIsSame(appGw *network.ApplicationGateway, cache *[]byte) bool {
 	// Keep a copy of the stripped JSON string
 	*cache = stripped
 
-	fmt.Printf(">>>>> cache:  %p -- %s\n\n", cache, *cache)
-	fmt.Printf(">>>>> strip:  %p -- %s\n\n", &stripped, stripped)
-
 	return sameAsCache
+}
+
+func isMap(v interface{}) bool {
+	return reflect.ValueOf(v).Type().Kind() == reflect.Map
+}
+
+func isSlice(v interface{}) bool {
+	return reflect.ValueOf(v).Type().Kind() == reflect.Slice
 }
 
 // deleteKey recursively deletes the given key from the map. This is NOT cap sensitive.
 func deleteKey(m *map[string]interface{}, keyToDelete string) {
+	// Recursively search for the given keyToDelete
 	for k, v := range *m {
 		if strings.ToLower(k) == strings.ToLower(keyToDelete) {
 			delete(*m, k)
-		} else if reflect.ValueOf(v).Type().Kind() == reflect.Map {
+			continue
+		}
+
+		// Recurse into other maps we find
+		if isMap(v) {
 			subMap := v.(map[string]interface{})
 			deleteKey(&subMap, keyToDelete)
-		} else if reflect.ValueOf(v).Type().Kind() == reflect.Slice {
-			valuesSlice := v.([]interface{})
-			for idx := range valuesSlice {
-				if reflect.ValueOf(valuesSlice[idx]).Type().Kind() == reflect.Map {
-					subMap := valuesSlice[idx].(map[string]interface{})
+			continue
+		}
+
+		// JSON blob could have a list for a value; Iterate and  find maps; Delete matching keys
+		if isSlice(v) {
+			slice := v.([]interface{})
+			for idx := range slice {
+				// We are not interested in any other element type but maps
+				if isMap(slice[idx]) {
+					subMap := slice[idx].(map[string]interface{})
 					deleteKey(&subMap, keyToDelete)
 				}
 			}
@@ -61,11 +75,11 @@ func deleteKey(m *map[string]interface{}, keyToDelete string) {
 
 // deleteKeyFromJSON assumes the []byte passed is JSON. It unmarshalls, deletes the given key, and marshalls again.
 func deleteKeyFromJSON(jsonWithEtag []byte, keyToDelete string) ([]byte, error) {
-	var stripped map[string]interface{}
-	if err := json.Unmarshal([]byte(jsonWithEtag), &stripped); err != nil {
+	var m map[string]interface{}
+	if err := json.Unmarshal([]byte(jsonWithEtag), &m); err != nil {
 		glog.Error("Could not unmarshal config App Gwy JSON to delete Etag.", err)
 		return nil, err
 	}
-	deleteKey(&stripped, keyToDelete)
-	return json.Marshal(stripped)
+	deleteKey(&m, keyToDelete)
+	return json.Marshal(m)
 }
