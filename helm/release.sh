31a38f891030cfeba4d09ed39d2286e3ebe7ce50
Delyan Raychev <delyan.raychev@microsoft.com>
2019-05-22 19:45:13 -0700

    Automate / script Helm chart releases

--- helm/release.sh
+++ helm/release.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-set -e
+
+set -eauo pipefail
+
+GIT_TAG=$(git describe --abbrev=0 --tags)
+
+TGZ_FILE=(ingress-azure-$GIT_TAG.tgz)
+
+if [ -f $TGZ_FILE ]; then
+  echo "File $TGZ_FILE already exists!"
+  exit 0
+fi
+
 echo " - running helm package"
-helm package ingress-azure --version 0.4.0
+helm package ingress-azure --version "$GIT_TAG"
+
 echo " - updating helm repo index"
 helm repo index . --url https://azure.github.io/application-gateway-kubernetes-ingress/helm
+
+echo " - update helm templates"
+cat ingress-azure/Chart-template.yaml | sed "s/XXVERSIONXX/$GIT_TAG/g" > ingress-azure/Chart.yaml
+cat ingress-azure/values-template.yaml | sed "s/XXVERSIONXX/$GIT_TAG/g" > ingress-azure/values.yaml
+./update-index.py
+
 echo " - done!"
