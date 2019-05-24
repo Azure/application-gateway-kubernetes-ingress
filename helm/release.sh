dac81881b038960cba3c21ef3dd33829166620df
Delyan Raychev <delyan.raychev@microsoft.com>
2019-05-23 17:47:24 -0700

    Bash shellcheck fixes

--- helm/release.sh
+++ helm/release.sh
@@ -4,22 +4,21 @@ set -eauo pipefail
 
 GIT_TAG=$(git describe --abbrev=0 --tags)
 
-TGZ_FILE=(ingress-azure-$GIT_TAG.tgz)
+TGZ_FILE="ingress-azure-$GIT_TAG.tgz"
 
-if [ -f $TGZ_FILE ]; then
+if [ -f "$TGZ_FILE" ]; then
   echo "File $TGZ_FILE already exists!"
   exit 0
 fi
 
 echo " - update helm templates"
-cat ingress-azure/Chart-template.yaml | sed "s/XXVERSIONXX/$GIT_TAG/g" > ingress-azure/Chart.yaml
-cat ingress-azure/values-template.yaml | sed "s/XXVERSIONXX/$GIT_TAG/g" > ingress-azure/values.yaml
+sed "s/XXVERSIONXX/$GIT_TAG/g" ingress-azure/Chart-template.yaml > ingress-azure/Chart.yaml
+sed "s/XXVERSIONXX/$GIT_TAG/g" ingress-azure/values-template.yaml > ingress-azure/values.yaml
 
 echo " - running helm package"
 helm package ingress-azure --version "$GIT_TAG"
 
 echo " - updating helm repo index"
 helm repo index . --url https://azure.github.io/application-gateway-kubernetes-ingress/helm
-
 
 echo " - done!"
