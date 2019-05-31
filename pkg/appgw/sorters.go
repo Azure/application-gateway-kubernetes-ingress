54eb05dd02d75e92f4fe9af7378060d397ee3620
Delyan Raychev <delyan.raychev@microsoft.com>
2019-05-30 21:38:53 -0800

    Fix ApplicationGatewayBackendAddress sorter

--- pkg/appgw/sorters.go
+++ pkg/appgw/sorters.go
@@ -6,6 +6,7 @@
 package appgw
 
 import (
+	"fmt"
 	n "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-12-01/network"
 )
 
@@ -15,10 +16,17 @@ type byIPFQDN []n.ApplicationGatewayBackendAddress
 func (a byIPFQDN) Len() int      { return len(a) }
 func (a byIPFQDN) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a byIPFQDN) Less(i, j int) bool {
-	if a[i].IPAddress != nil && a[j].IPAddress != nil && len(*a[i].IPAddress) > 0 {
-		return *a[i].IPAddress < *a[j].IPAddress
-	} else if a[i].Fqdn != nil && a[j].Fqdn != nil && len(*a[i].Fqdn) != 0 {
-		return *a[i].Fqdn < *a[j].Fqdn
+	return getIPFQDNKey(a[i]) < getIPFQDNKey(a[j])
+}
+
+func getIPFQDNKey(record n.ApplicationGatewayBackendAddress) string {
+	fqdn := ""
+	if record.Fqdn != nil {
+		fqdn = *record.Fqdn
 	}
-	return false
+	ip := ""
+	if record.IPAddress != nil {
+		ip = *record.IPAddress
+	}
+	return fmt.Sprintf("%s-%s", fqdn, ip)
 }
