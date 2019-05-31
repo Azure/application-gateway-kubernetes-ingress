9b753eb3eb9b6b8df35cd9a73fefb67e63a21919
Delyan Raychev <delyan.raychev@microsoft.com>
2019-05-31 13:29:06 -0700

    go fmt

--- pkg/tests/types.go
+++ pkg/tests/types.go
@@ -6,16 +6,19 @@ import (
 )
 
 // MockEventRecorder mocks EventRecorder, which knows how to record events on behalf of an EventSource.
-type MockEventRecorder struct {}
+type MockEventRecorder struct{}
 
 // Event constructs an event from the given information and puts it in the queue for sending.
 func (e MockEventRecorder) Event(object runtime.Object, eventtype, reason, message string) {}
 
 // Eventf is just like Event, but with Sprintf for the message field.
-func (e MockEventRecorder) Eventf(object runtime.Object, eventtype, reason, messageFmt string, args ...interface{}) {}
+func (e MockEventRecorder) Eventf(object runtime.Object, eventtype, reason, messageFmt string, args ...interface{}) {
+}
 
 // PastEventf is just like Eventf, but with an option to specify the event's 'timestamp' field.
-func (e MockEventRecorder) PastEventf(object runtime.Object, timestamp v1.Time, eventtype, reason, messageFmt string, args ...interface{}) {}
+func (e MockEventRecorder) PastEventf(object runtime.Object, timestamp v1.Time, eventtype, reason, messageFmt string, args ...interface{}) {
+}
 
 // AnnotatedEventf is just like eventf, but with annotations attached
-func (e MockEventRecorder) AnnotatedEventf(object runtime.Object, annotations map[string]string, eventtype, reason, messageFmt string, args ...interface{}) {}
+func (e MockEventRecorder) AnnotatedEventf(object runtime.Object, annotations map[string]string, eventtype, reason, messageFmt string, args ...interface{}) {
+}
