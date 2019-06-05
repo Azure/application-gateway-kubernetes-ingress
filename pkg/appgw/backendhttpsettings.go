3e9c72b46290fa13839c6fdcf35e70025281e547
Rhea <rheaj99@gmail.com>
2019-06-05 13:41:56 -0700

    created new function to get backends, backendHTTPSettingsMap, and serviceBackendPairMap

--- pkg/appgw/backendhttpsettings.go
+++ pkg/appgw/backendhttpsettings.go
@@ -23,6 +23,150 @@ const (
 	DefaultConnDrainTimeoutInSec = 30
 )
 
+func (c *appGwConfigBuilder) getBackendsAndSettingsMap(ingressList []*v1beta1.Ingress) (*[]network.ApplicationGatewayBackendHTTPSettings, map[backendIdentifier]*network.ApplicationGatewayBackendHTTPSettings, map[backendIdentifier]serviceBackendPortPair, error) {
+	backendIDs := make(map[backendIdentifier]interface{})
+	serviceBackendPairsMap := make(map[backendIdentifier]map[serviceBackendPortPair]interface{})
+	backendHTTPSettingsMap := make(map[backendIdentifier]*network.ApplicationGatewayBackendHTTPSettings)
+	finalServiceBackendPairMap := make(map[backendIdentifier]serviceBackendPortPair)
+
+	for _, ingress := range ingressList {
+		defIngressBackend := ingress.Spec.Backend
+		if defIngressBackend != nil {
+			backendIDs[generateBackendID(ingress, nil, nil, defIngressBackend)] = nil
+		}
+		for ruleIdx := range ingress.Spec.Rules {
+			rule := &ingress.Spec.Rules[ruleIdx]
+			if rule.HTTP == nil {
+				// skip no http rule
+				continue
+			}
+			for pathIdx := range rule.HTTP.Paths {
+				path := &rule.HTTP.Paths[pathIdx]
+				backendIDs[generateBackendID(ingress, rule, path, &path.Backend)] = nil
+			}
+		}
+	}
+
+	var unresolvedBackendID []backendIdentifier
+	for backendID := range backendIDs {
+		resolvedBackendPorts := make(map[serviceBackendPortPair]interface{})
+
+		service := c.k8sContext.GetService(backendID.serviceKey())
+		if service == nil {
+			logLine := fmt.Sprintf("Unable to get the service [%s]", backendID.serviceKey())
+			c.recorder.Event(backendID.Ingress, v1.EventTypeWarning, "ServiceNotFound", logLine)
+			glog.Errorf(logLine)
+			pair := serviceBackendPortPair{
+				ServicePort: backendID.Backend.ServicePort.IntVal,
+				BackendPort: backendID.Backend.ServicePort.IntVal,
+			}
+			resolvedBackendPorts[pair] = nil
+		} else {
+			for _, sp := range service.Spec.Ports {
+				// find the backend port number
+				// check if any service ports matches the specified ports
+				if sp.Protocol != v1.ProtocolTCP {
+					// ignore UDP ports
+					continue
+				}
+				if fmt.Sprint(sp.Port) == backendID.Backend.ServicePort.String() ||
+					sp.Name == backendID.Backend.ServicePort.String() ||
+					sp.TargetPort.String() == backendID.Backend.ServicePort.String() {
+					// matched a service port with a port from the service
+
+					if sp.TargetPort.String() == "" {
+						// targetPort is not defined, by default targetPort == port
+						pair := serviceBackendPortPair{
+							ServicePort: sp.Port,
+							BackendPort: sp.Port,
+						}
+						resolvedBackendPorts[pair] = nil
+					} else {
+						// target port is defined as name or port number
+						if sp.TargetPort.Type == intstr.Int {
+							// port is defined as port number
+							pair := serviceBackendPortPair{
+								ServicePort: sp.Port,
+								BackendPort: sp.TargetPort.IntVal,
+							}
+							resolvedBackendPorts[pair] = nil
+						} else {
+							// if service port is defined by name, need to resolve
+							targetPortName := sp.TargetPort.StrVal
+							glog.V(1).Infof("resolving port name %s", targetPortName)
+							targetPortsResolved := c.resolvePortName(targetPortName, &backendID)
+							for targetPort := range targetPortsResolved {
+								pair := serviceBackendPortPair{
+									ServicePort: sp.Port,
+									BackendPort: targetPort,
+								}
+								resolvedBackendPorts[pair] = nil
+							}
+						}
+					}
+					break
+				}
+			}
+		}
+
+		if len(resolvedBackendPorts) == 0 {
+			logLine := fmt.Sprintf("Unable to resolve any backend port for service [%s]", backendID.serviceKey())
+			c.recorder.Event(backendID.Ingress, v1.EventTypeWarning, "PortResolutionError", logLine)
+			glog.Error(logLine)
+
+			unresolvedBackendID = append(unresolvedBackendID, backendID)
+			break
+		}
+
+		// Merge serviceBackendPairsMap[backendID] into resolvedBackendPorts
+		if _, ok := serviceBackendPairsMap[backendID]; !ok {
+			serviceBackendPairsMap[backendID] = make(map[serviceBackendPortPair]interface{})
+		}
+		for portPair := range resolvedBackendPorts {
+			serviceBackendPairsMap[backendID][portPair] = nil
+		}
+	}
+
+	if len(unresolvedBackendID) > 0 {
+		return nil, backendHTTPSettingsMap, finalServiceBackendPairMap, errors.New("unable to resolve backend port for some services")
+	}
+
+	probeID := c.appGwIdentifier.probeID(defaultProbeName)
+	httpSettingsCollection := make(map[string]network.ApplicationGatewayBackendHTTPSettings)
+	defaultBackend := defaultBackendHTTPSettings(probeID)
+	httpSettingsCollection[*defaultBackend.Name] = defaultBackend
+
+	// enforce single pair relationship between service port and backend port
+	for backendID, serviceBackendPairs := range serviceBackendPairsMap {
+		if len(serviceBackendPairs) > 1 {
+			// more than one possible backend port exposed through ingress
+			logLine := fmt.Sprintf("service:port [%s:%s] has more than one service-backend port binding",
+				backendID.serviceKey(), backendID.Backend.ServicePort.String())
+			c.recorder.Event(backendID.Ingress, v1.EventTypeWarning, "PortResolutionError", logLine)
+			glog.Warning(logLine)
+			return nil, backendHTTPSettingsMap, finalServiceBackendPairMap, errors.New("more than one service-backend port binding is not allowed")
+		}
+
+		// At this point there will be only one pair
+		var uniquePair serviceBackendPortPair
+		for k := range serviceBackendPairs {
+			uniquePair = k
+		}
+
+		finalServiceBackendPairMap[backendID] = uniquePair
+		httpSettings := c.generateHTTPSettings(backendID, uniquePair.BackendPort)
+		httpSettingsCollection[*httpSettings.Name] = httpSettings
+		backendHTTPSettingsMap[backendID] = &httpSettings
+	}
+
+	backends := make([]network.ApplicationGatewayBackendHTTPSettings, 0, len(httpSettingsCollection))
+	for _, backend := range httpSettingsCollection {
+		backends = append(backends, backend)
+	}
+
+	return &backends, backendHTTPSettingsMap, finalServiceBackendPairMap, nil
+}
+
 func (c *appGwConfigBuilder) BackendHTTPSettingsCollection(ingressList []*v1beta1.Ingress) error {
 	backendIDs := make(map[backendIdentifier]interface{})
 	serviceBackendPairsMap := make(map[backendIdentifier]map[serviceBackendPortPair]interface{})
