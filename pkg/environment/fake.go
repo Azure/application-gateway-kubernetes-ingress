7c8d227480cb3b81a3fb565cc2eea3f9fd6a2d60
Akshay Gupta <aksgupta@microsoft.com>
2019-06-14 10:01:56 -0700

    new env package

---
+++ pkg/environment/fake.go
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+package environment
+
+// GetFakeEnv returns fake values for defined environment variables for Ingress Controller.
+func GetFakeEnv() EnvVariables {
+	env := EnvVariables{
+		SubscriptionID:    "--SubscriptionID--",
+		ResourceGroupName: "--ResourceGroupName--",
+		AppGwName:         "--AppGwName--",
+		AuthLocation:      "--AuthLocation--",
+		WatchNamespace:    "--WatchNamespace--",
+		UsePrivateIP:      "false",
+	}
+
+	return env
+}
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -7,12 +7,8 @@ package utils
 
 import (
 	"fmt"
-	"os"
-	"regexp"
 	"strconv"
 	"strings"
-
-	"github.com/golang/glog"
 )
 
 // MaxInt64 returns the greater one of the two
@@ -43,18 +39,4 @@ func IntsToString(l []int, delim string) string {
 // GetResourceKey generates the key in k8s format for a given resource
 func GetResourceKey(namespace, name string) string {
 	return fmt.Sprintf("%v/%v", namespace, name)
-}
-
-// GetEnv is an augmentation of os.Getenv, providing it with a default value.
-func GetEnv(environmentVariable, defaultValue string, validator *regexp.Regexp) string {
-	if value, ok := os.LookupEnv(environmentVariable); ok {
-		if validator == nil {
-			return value
-		}
-		if validator.MatchString(value) {
-			return value
-		}
-		glog.Errorf("Environment variable %s contains a value which does not pass validation filter; Using default value: %s", environmentVariable, defaultValue)
-	}
-	return defaultValue
 }
