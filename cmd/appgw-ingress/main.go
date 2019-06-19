4c887a541a7c1ed249c21f477a377091548aee99
Delyan Raychev <delyan.raychev@microsoft.com>
2019-06-19 16:04:02 -0700

    Adding VERBOSITY_LEVEL env var; Add to helm template

--- cmd/appgw-ingress/main.go
+++ cmd/appgw-ingress/main.go
@@ -37,6 +37,11 @@ import (
 	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/version"
 )
 
+const (
+	verbosityFlag = "verbosity"
+	maxAuthRetry  = 10
+)
+
 var (
 	flags = pflag.NewFlagSet(`appgw-ingress`, pflag.ExitOnError)
 
@@ -54,7 +59,7 @@ var (
 
 	versionInfo = flags.Bool("version", false, "Print version")
 
-	verbosity = flags.Int("verbosity", 1, "Set logging verbosity level")
+	verbosity = flags.Int(verbosityFlag, 1, "Set logging verbosity level")
 )
 
 func main() {
@@ -64,7 +69,10 @@ func main() {
 		glog.Fatal("Error parsing command line arguments:", err)
 	}
 
-	glog.Infof("Logging at verbosity level %d", *verbosity)
+	env := environment.GetEnv()
+	environment.ValidateEnv(env)
+
+	glog.Infof("Logging at verbosity level %d", getVerbosity(*verbosity, env.VerbosityLevel))
 
 	if *versionInfo {
 		version.PrintVersionAndExit()
@@ -75,9 +83,6 @@ func main() {
 	_ = flag.CommandLine.Parse([]string{})
 	_ = flag.Lookup("logtostderr").Value.Set("true")
 	_ = flag.Set("v", strconv.Itoa(*verbosity))
-
-	env := environment.GetEnv()
-	environment.ValidateEnv(env)
 
 	appGwClient := network.NewApplicationGatewaysClient(env.SubscriptionID)
 
@@ -158,9 +163,8 @@ func getAzAuth(vars environment.EnvVariables) (autorest.Authorizer, error) {
 }
 
 func waitForAzureAuth(envVars environment.EnvVariables, client network.ApplicationGatewaysClient) {
-	maxRetry := 10
 	const retryTime = 10 * time.Second
-	for counter := 0; counter <= maxRetry; counter++ {
+	for counter := 0; counter <= maxAuthRetry; counter++ {
 		if _, err := client.Get(context.Background(), envVars.ResourceGroupName, envVars.AppGwName); err != nil {
 			glog.Error("Error getting Application Gateway", envVars.AppGwName, err)
 			glog.Infof("Retrying in %v", retryTime)
@@ -208,4 +212,14 @@ func getEventRecorder(kubeClient kubernetes.Interface) record.EventRecorder {
 		Host:      hostname,
 	}
 	return eventBroadcaster.NewRecorder(scheme.Scheme, source)
+}
+
+func getVerbosity(flagVerbosity int, envVerbosity string) int {
+	envVerbosityInt, err := strconv.Atoi(envVerbosity)
+	if err != nil {
+		glog.Infof("Using verbosity level %d from CLI flag %s", flagVerbosity, verbosityFlag)
+		return flagVerbosity
+	}
+	glog.Infof("Using verbosity level %d from environment variable %s", envVerbosityInt, environment.VerbosityLevel)
+	return envVerbosityInt
 }
