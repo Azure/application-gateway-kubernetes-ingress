f87d7234b0d8eb750e5cd58162ea86f7c9513515
Akshay Gupta <aksgupta@microsoft.com>
2019-07-01 15:24:38 -0700

    eventqueue: simple worker (#341)

---
+++ pkg/worker/fake.go
@@ -0,0 +1,27 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+package worker
+
+import (
+	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/events"
+)
+
+// FakeProcessor is fake event processor type
+type FakeProcessor struct {
+	processFunc func(events.Event) error
+}
+
+// Process will call the callback provided
+func (fp FakeProcessor) Process(event events.Event) error {
+	return fp.processFunc(event)
+}
+
+// NewFakeProcessor returns a fake processor struct.
+func NewFakeProcessor(process func(events.Event) error) FakeProcessor {
+	return FakeProcessor{
+		processFunc: process,
+	}
+}
