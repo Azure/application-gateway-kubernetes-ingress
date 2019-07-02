b57dcbde8a2725c7ad0c8639fab0123839c20cc0
Rhea <rheaj99@gmail.com>
2019-07-02 16:13:31 -0700

    get virtual services function for istio

--- pkg/istio/controller/virtual_services.go
+++ pkg/istio/controller/virtual_services.go
@@ -4,5 +4,15 @@ import "github.com/knative/pkg/apis/istio/v1alpha3"
 
 // GetVirtualServicesForGateway returns the VirtualServices for the provided gateway
 func GetVirtualServicesForGateway(gateway v1alpha3.Gateway) []*v1alpha3.VirtualService {
-	return []*v1alpha3.VirtualService{}
+	virtualServices := make([]*v1alpha3.VirtualService, 0)
+	allVirtualServices := make([]*v1alpha3.VirtualService, 0) /* TO DO - get all virtual services and replace this */
+	gatewayName := gateway.Name
+	for _, service := range allVirtualServices {
+		for _, serviceGateway := range service.Spec.Gateways {
+			if gatewayName == serviceGateway {
+				virtualServices = append(virtualServices, service)
+			}
+		}
+	}
+	return virtualServices
 }
