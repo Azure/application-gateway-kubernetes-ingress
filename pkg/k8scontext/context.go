2b2356012ef022c82b781eed65bc8941503b6136
Rhea <rheaj99@gmail.com>
2019-07-02 16:51:51 -0700

    added virtual service to app gw config

--- pkg/k8scontext/context.go
+++ pkg/k8scontext/context.go
@@ -281,6 +281,14 @@ func (c *Context) ListIstioGateways() []*v1alpha3.Gateway {
 	return gateways
 }
 
+func (c *Context) ListIstioVirtualServices() []*v1alpha3.VirtualService {
+	var virtualServices []*v1alpha3.VirtualService
+	for _, virtualService := range c.Caches.IstioVirtualService.List() {
+		virtualServices = append(virtualServices, virtualService.(*v1alpha3.VirtualService))
+	}
+	return virtualServices
+}
+
 // GetService returns the service identified by the key.
 func (c *Context) GetService(serviceKey string) *v1.Service {
 	serviceInterface, exist, err := c.Caches.Service.GetByKey(serviceKey)
