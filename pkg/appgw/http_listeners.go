160a48f5a03523f436a7d79449b3deb5926f7a9a
Delyan Raychev <49918230+draychev@users.noreply.github.com>
2019-07-03 16:34:10 -0700

    Reuse ConfigBuilderContext to simplify (#353)

--- pkg/appgw/http_listeners.go
+++ pkg/appgw/http_listeners.go
@@ -7,14 +7,14 @@ package appgw
 
 func (c *appGwConfigBuilder) Listeners(cbCtx *ConfigBuilderContext) error {
 
-	c.appGw.SslCertificates = c.getSslCertificates(cbCtx.IngressList)
-	c.appGw.FrontendPorts = c.getFrontendPorts(cbCtx.IngressList)
+	c.appGw.SslCertificates = c.getSslCertificates(cbCtx)
+	c.appGw.FrontendPorts = c.getFrontendPorts(cbCtx)
 	c.appGw.HTTPListeners, _ = c.getListeners(cbCtx)
 
 	// App Gateway Rules can be configured to redirect HTTP traffic to HTTPS URLs.
 	// In this step here we create the redirection configurations. These configs are attached to request routing rules
 	// in the RequestRoutingRules step, which must be executed after Listeners.
-	c.appGw.RedirectConfigurations = c.getRedirectConfigurations(cbCtx.IngressList)
+	c.appGw.RedirectConfigurations = c.getRedirectConfigurations(cbCtx)
 
 	return nil
 }
