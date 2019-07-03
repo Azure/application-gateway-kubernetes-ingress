f50bff30e9518b026fbbbd98975ffc230ef77d81
Rhea <rheaj99@gmail.com>
2019-07-03 09:42:35 -0700

    edited virtual services function to get all services from k8s context

--- pkg/istio/controller/virtual_services.go
+++ pkg/istio/controller/virtual_services.go
@@ -1,11 +1,12 @@
 package controller
 
 import "github.com/knative/pkg/apis/istio/v1alpha3"
+import "github.com/Azure/application-gateway-kubernetes-ingress/pkg/k8scontext"
 
 // GetVirtualServicesForGateway returns the VirtualServices for the provided gateway
-func GetVirtualServicesForGateway(gateway v1alpha3.Gateway) []*v1alpha3.VirtualService {
+func GetVirtualServicesForGateway(gateway v1alpha3.Gateway, c *k8scontext.Context) []*v1alpha3.VirtualService {
 	virtualServices := make([]*v1alpha3.VirtualService, 0)
-	allVirtualServices := make([]*v1alpha3.VirtualService, 0) /* TO DO - get all virtual services and replace this */
+	allVirtualServices := c.ListIstioVirtualServices()
 	gatewayName := gateway.Name
 	for _, service := range allVirtualServices {
 		for _, serviceGateway := range service.Spec.Gateways {
