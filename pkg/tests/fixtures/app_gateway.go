f15a477910c6bfc9965a1f1cb7bf066512b510c9
Delyan Raychev <delyan.raychev@microsoft.com>
2019-07-16 08:49:43 -0700

    brownfield: Adding test fixtures

---
+++ pkg/tests/fixtures/app_gateway.go
@@ -0,0 +1,51 @@
+package fixtures
+
+import (
+	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/tests"
+	n "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-12-01/network"
+	"github.com/Azure/go-autorest/autorest/to"
+)
+
+func GetAppGateway() n.ApplicationGateway {
+	// The order of the lists below is important as we reference these by index in unit tests.
+	return n.ApplicationGateway{
+		ApplicationGatewayPropertiesFormat: &n.ApplicationGatewayPropertiesFormat{
+
+			RequestRoutingRules: &[]n.ApplicationGatewayRequestRoutingRule{
+				*GetDefaultRoutingRule(),
+				*GetRequestRoutingRuleBasic(),
+				*GetRequestRoutingRulePathBased1(),
+				*GetRequestRoutingRulePathBased2(),
+			},
+			URLPathMaps: &[]n.ApplicationGatewayURLPathMap{
+				*GetDefaultURLPathMap(),
+				*GetURLPathMap1(),
+				*GetURLPathMap2(),
+			},
+
+			HTTPListeners: &[]n.ApplicationGatewayHTTPListener{
+				*GetListenerPathBased1(),
+				*GetListenerPathBased2(),
+				*GetListenerBasic(),
+			},
+
+			SslCertificates: &[]n.ApplicationGatewaySslCertificate{
+				GetCertificate1(),
+				GetCertificate2(),
+				GetCertificate3(),
+			},
+
+			Probes: &[]n.ApplicationGatewayProbe{
+				GetApplicationGatewayProbe(nil, to.StringPtr(PathFoo)), // /foo
+				GetApplicationGatewayProbe(nil, to.StringPtr(PathBar)), // /bar
+				GetApplicationGatewayProbe(to.StringPtr(tests.OtherHost), nil),
+			},
+
+			BackendHTTPSettingsCollection: &[]n.ApplicationGatewayBackendHTTPSettings{
+				GetHTTPSettings1(),
+				GetHTTPSettings2(),
+				GetHTTPSettings3(),
+			},
+		},
+	}
+}
