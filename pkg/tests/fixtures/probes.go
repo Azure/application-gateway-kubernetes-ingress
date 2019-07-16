822d8e1cd4a4d59e0fbb737213487ffb2f4b42aa
Delyan Raychev <49918230+draychev@users.noreply.github.com>
2019-07-16 12:18:35 -0700

    brownfield: Adding test fixtures (#375)

--- pkg/tests/fixtures/probes.go
+++ pkg/tests/fixtures/probes.go
@@ -6,10 +6,19 @@
 package fixtures
 
 import (
+	"encoding/base64"
+	"fmt"
 	n "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-12-01/network"
 	"github.com/Azure/go-autorest/autorest/to"
+	"strings"
 
 	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/tests"
+)
+
+const (
+	ProbeName1 = "probe-name-YnllLmNvbQ-L2Zvbw"                      // fixtures.Host  fixtures.PathFoo
+	ProbeName2 = "probe-name-YnllLmNvbQ-L2Jhcg"                      // fixtures.Host  fixtures.PathBar
+	ProbeName3 = "probe-name-LS1zb21lLW90aGVyLWhvc3RuYW1lLS0-L2Zvbw" // fixtures.OtherHost  fixtures.PathFoo
 )
 
 // GetApplicationGatewayProbe creates a new struct for use in unit tests.
@@ -20,13 +29,18 @@ func GetApplicationGatewayProbe(host *string, path *string) n.ApplicationGateway
 	if path == nil {
 		path = to.StringPtr("/foo")
 	}
+
+	encodedHost := strings.TrimRight(base64.StdEncoding.EncodeToString([]byte(*host)), "=")
+	encodedPath := strings.TrimRight(base64.StdEncoding.EncodeToString([]byte(*path)), "=")
+	probeName := to.StringPtr(fmt.Sprintf("probe-name-%s-%s", encodedHost, encodedPath))
+
 	return n.ApplicationGatewayProbe{
 		ApplicationGatewayProbePropertiesFormat: &n.ApplicationGatewayProbePropertiesFormat{
 			Protocol: n.HTTPS,
 			Host:     host,
 			Path:     path,
 		},
-		Name: to.StringPtr("probe-name"),
+		Name: probeName,
 		ID:   to.StringPtr("abcd"),
 	}
 }
