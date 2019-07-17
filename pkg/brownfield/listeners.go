4763fa07996ab22db3c505e3b7b67f026868c685
Delyan Raychev <49918230+draychev@users.noreply.github.com>
2019-07-17 12:56:50 -0700

    brownfield: HTTP Listeners (#381)

--- pkg/brownfield/listeners.go
+++ pkg/brownfield/listeners.go
@@ -6,10 +6,86 @@
 package brownfield
 
 import (
+	"strings"
+
 	n "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-12-01/network"
+	"github.com/golang/glog"
+
+	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/utils"
 )
 
 type listenersByName map[listenerName]n.ApplicationGatewayHTTPListener
+
+// GetBlacklistedListeners filters the given list of health probes to the list Probes that AGIC is allowed to manage.
+func (er ExistingResources) GetBlacklistedListeners() ([]n.ApplicationGatewayHTTPListener, []n.ApplicationGatewayHTTPListener) {
+	blacklistedListenersSet := er.getBlacklistedListenersSet()
+	var blacklisted, nonBlacklisted []n.ApplicationGatewayHTTPListener
+	for _, listener := range er.Listeners {
+		listenerNm := listenerName(*listener.Name)
+		if _, exists := blacklistedListenersSet[listenerNm]; exists {
+			glog.V(5).Infof("[brownfield] Listener %s is blacklisted", listenerNm)
+			blacklisted = append(blacklisted, listener)
+			continue
+		}
+		glog.V(5).Infof("[brownfield] Listener %s is not blacklisted", listenerNm)
+		nonBlacklisted = append(nonBlacklisted, listener)
+	}
+	return blacklisted, nonBlacklisted
+}
+
+// MergeListeners merges list of lists of listeners into a single list, maintaining uniqueness.
+func MergeListeners(listenerBuckets ...[]n.ApplicationGatewayHTTPListener) []n.ApplicationGatewayHTTPListener {
+	uniq := make(listenersByName)
+	for _, bucket := range listenerBuckets {
+		for _, listener := range bucket {
+			uniq[listenerName(*listener.Name)] = listener
+		}
+	}
+	var merged []n.ApplicationGatewayHTTPListener
+	for _, listener := range uniq {
+		merged = append(merged, listener)
+	}
+	return merged
+}
+
+// LogListeners emits a few log lines detailing what Listeners are created, blacklisted, and removed from ARM.
+func LogListeners(existingBlacklisted []n.ApplicationGatewayHTTPListener, existingNonBlacklisted []n.ApplicationGatewayHTTPListener, managedListeners []n.ApplicationGatewayHTTPListener) {
+	var garbage []n.ApplicationGatewayHTTPListener
+
+	blacklistedSet := indexListenersByName(existingBlacklisted)
+	managedSet := indexListenersByName(managedListeners)
+
+	for listenerName, listener := range indexListenersByName(existingNonBlacklisted) {
+		_, existsInBlacklist := blacklistedSet[listenerName]
+		_, existsInNewListeners := managedSet[listenerName]
+		if !existsInBlacklist && !existsInNewListeners {
+			garbage = append(garbage, listener)
+		}
+	}
+
+	glog.V(3).Info("[brownfield] Listeners AGIC created: ", getListenerNames(managedListeners))
+	glog.V(3).Info("[brownfield] Existing Blacklisted Listeners AGIC will retain: ", getListenerNames(existingBlacklisted))
+	glog.V(3).Info("[brownfield] Existing Listeners AGIC will remove: ", getListenerNames(garbage))
+}
+
+func getListenerNames(listeners []n.ApplicationGatewayHTTPListener) string {
+	var names []string
+	for _, p := range listeners {
+		names = append(names, *p.Name)
+	}
+	if len(names) == 0 {
+		return "n/a"
+	}
+	return strings.Join(names, ", ")
+}
+
+func indexListenersByName(listeners []n.ApplicationGatewayHTTPListener) listenersByName {
+	indexed := make(listenersByName)
+	for _, listener := range listeners {
+		indexed[listenerName(*listener.Name)] = listener
+	}
+	return indexed
+}
 
 // getListenersByName indexes listeners by their name
 func (er *ExistingResources) getListenersByName() listenersByName {
@@ -22,4 +98,16 @@ func (er *ExistingResources) getListenersByName() listenersByName {
 	}
 	er.listenersByName = listenersByName
 	return listenersByName
+}
+
+func (er ExistingResources) getBlacklistedListenersSet() map[listenerName]interface{} {
+	blacklistedRoutingRules, _ := er.GetBlacklistedRoutingRules()
+	blacklistedListenersSet := make(map[listenerName]interface{})
+	for _, rule := range blacklistedRoutingRules {
+		if rule.HTTPListener != nil && rule.HTTPListener.ID != nil {
+			listenerName := listenerName(utils.GetLastChunkOfSlashed(*rule.HTTPListener.ID))
+			blacklistedListenersSet[listenerName] = nil
+		}
+	}
+	return blacklistedListenersSet
 }
