d71871b63049667b2b08f343023431800d59e873
akshaysngupta <akshaysngupta@gmail.com>
2019-07-31 11:34:09 -0700

    fix test fixures

--- pkg/tests/fixtures.go
+++ pkg/tests/fixtures.go
@@ -43,6 +43,8 @@ const (
 	AppGwName        = "--app-gw-name--"
 	PublicIPID       = "--front-end-ip-id-1--"
 	PrivateIPID      = "--front-end-ip-id-2--"
+	ServiceHTTPPort  = "--service-http-port--"
+	ServiceHTTPSPort = "--service-https-port--"
 )
 
 // GetIngress creates an Ingress test fixture.
@@ -167,7 +169,7 @@ func NewServicePortsFixture() *[]v1.ServicePort {
 		// All ports within a ServiceSpec must have unique names. This maps to
 		// the 'Name' field in EndpointPort objects.
 		// Optional if only one ServicePort is defined on this service.
-		Name: "http",
+		Name: ServiceHTTPPort,
 
 		// The IP protocol for this port. Supports "TCP", "UDP", and "SCTP".
 		Protocol: v1.ProtocolTCP,
@@ -189,7 +191,7 @@ func NewServicePortsFixture() *[]v1.ServicePort {
 	}
 
 	httpsPort := v1.ServicePort{
-		Name:     "https",
+		Name:     ServiceHTTPSPort,
 		Protocol: v1.ProtocolTCP,
 		Port:     443,
 		TargetPort: intstr.IntOrString{
@@ -329,7 +331,12 @@ func NewEndpointsFixture() *v1.Endpoints {
 				Ports: []v1.EndpointPort{
 					{
 						Protocol: v1.ProtocolTCP,
-						Name:     Name,
+						Name:     ServiceHTTPPort,
+						Port:     ContainerPort,
+					},
+					{
+						Protocol: v1.ProtocolTCP,
+						Name:     ServiceHTTPSPort,
 						Port:     ContainerPort,
 					},
 				},
@@ -374,6 +381,53 @@ func NewIngressTestFixture(namespace string, ingressName string) v1beta1.Ingress
 	}
 }
 
+// NewIngressTestFixtureBasic creates a basic Ingress with / path for testing.
+func NewIngressTestFixtureBasic(namespace string, ingressName string, tls bool) *v1beta1.Ingress {
+	ingress := &v1beta1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      ingressName,
+			Namespace: namespace,
+			Annotations: map[string]string{
+				annotations.IngressClassKey: annotations.ApplicationGatewayIngressClass,
+			},
+		},
+		Spec: v1beta1.IngressSpec{
+			Rules: []v1beta1.IngressRule{
+				{
+					Host: "hello.com",
+					IngressRuleValue: v1beta1.IngressRuleValue{
+						HTTP: &v1beta1.HTTPIngressRuleValue{
+							Paths: []v1beta1.HTTPIngressPath{
+								{
+									Path: "/",
+									Backend: v1beta1.IngressBackend{
+										ServiceName: ServiceName,
+										ServicePort: intstr.IntOrString{
+											Type:   intstr.String,
+											StrVal: "http",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if tls {
+		ingress.Spec.TLS = []v1beta1.IngressTLS{
+			{
+				SecretName: NameOfSecret,
+			},
+		}
+		ingress.Annotations[annotations.SslRedirectKey] = "true"
+	}
+
+	return ingress
+}
+
 // NewPodTestFixture creates a new Pod struct for testing.
 func NewPodTestFixture(namespace string, podName string) v1.Pod {
 	return v1.Pod{
@@ -385,5 +439,19 @@ func NewPodTestFixture(namespace string, podName string) v1.Pod {
 			},
 		},
 		Spec: v1.PodSpec{},
+	}
+}
+
+// NewSecretTestFixture creates a new secret for testing
+func NewSecretTestFixture() *v1.Secret {
+	return &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      NameOfSecret,
+			Namespace: Namespace,
+		},
+		StringData: map[string]string{
+			"tls.crt": "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUNwakNDQVk0Q0NRRDE5NytIcnFITjZ6QU5CZ2txaGtpRzl3MEJBUXNGQURBVk1STXdFUVlEVlFRRERBcHoKWVcxd2JHVXRZWEJ3TUI0WERURTVNRGN6TURJd05Ua3lNRm9YRFRJd01EY3lPVEl3TlRreU1Gb3dGVEVUTUJFRwpBMVVFQXd3S2MyRnRjR3hsTFdGd2NEQ0NBU0l3RFFZSktvWklodmNOQVFFQkJRQURnZ0VQQURDQ0FRb0NnZ0VCCkFOUzFqYzFVdnhLTTVaQ09XRWRTWW0wVkUyNDNIMjkrek5Db1NpVnNFMXN4d1F5bVpuUk9PZVpyMnV5eUlUdmcKblpmZUJCN01qZllkcHJGSGVmR01EUEs4a2xGMGU5bHJNM0dGTjFaaFQwbmFGR2U3SjFHcS85Wk1CMTBXdjllZwpFTTVoMXRmdGdUMGNRbHhFaXJ0a3poQnVISDQycW5pa0U3ZGxsUHMxOTkra3ZudVBQTzBUaEZZQVRCYSt5NUs5Cm1BUmg1U2JyK2JscXJndCt0Zzl0T2RuT2hlK2tyMytuVnBBcGdJZERPSHplVXVrWlpNcU90anZBTGJMRDlmeWYKNlBOMDVXSjBFcDROMWZhZmIzclpEMXZVcnd4OUxxMEpwb0NNeUxzMXVaWGhyWk1IK3dKaUZ5akYzR2dXRDVIUwp6Y2I0K2R1cFFpdWY5WjZNT3Y4d1JDc0NBd0VBQVRBTkJna3Foa2lHOXcwQkFRc0ZBQU9DQVFFQUQ3aVBpUGh5CnNhR1FpYTJUUGIvM24yVi83cWVhUUxFYXQ3R3h0UTVDTHpkZXhhL0tnV3RiV0RYTWRneGVTOElOWVpFSUxsck8KaUFtZTRXdTBZeUYrQUdBRjBCVkhxaWJDV1RFb1FmZ2p6S3ZYandpZ1VmWXdhL1ZETWpnYzNybktTbDVvVEhTegpmbE5DMHVqaDk1YWJyNVlDQ1FUdFpUYUh3NU44VmhpamhnUnlDQmtXd2xIZWl0MHVSbEdvS3VIUHh2dVN3djM0CmZ3Uy9GbkcvM0tkc0RtZENDWWYrSm9rbDhHZXFzejNGR3dKWWV1S3pXL0J2UlVxaEdxRlVQVjRNditOVEY2TDkKNXhpRWlnblo1WS9HbjM0aXhWTTRPVldLL0lYUy9vUXFDWkJQWnJDb2pwMnpyMWw4SnVRUU5WQ1lDbEpoajlJTApVNXlteWRBUDVOaStCUT09Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K",
+			"tls.key": "LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tCk1JSUV3QUlCQURBTkJna3Foa2lHOXcwQkFRRUZBQVNDQktvd2dnU21BZ0VBQW9JQkFRRFV0WTNOVkw4U2pPV1EKamxoSFVtSnRGUk51Tng5dmZzelFxRW9sYkJOYk1jRU1wbVowVGpubWE5cnNzaUU3NEoyWDNnUWV6STMySGFheApSM254akF6eXZKSlJkSHZaYXpOeGhUZFdZVTlKMmhSbnV5ZFJxdi9XVEFkZEZyL1hvQkRPWWRiWDdZRTlIRUpjClJJcTdaTTRRYmh4K05xcDRwQk8zWlpUN05mZmZwTDU3anp6dEU0UldBRXdXdnN1U3ZaZ0VZZVVtNi9tNWFxNEwKZnJZUGJUblp6b1h2cEs5L3AxYVFLWUNIUXpoODNsTHBHV1RLanJZN3dDMnl3L1g4bitqemRPVmlkQktlRGRYMgpuMjk2MlE5YjFLOE1mUzZ0Q2FhQWpNaTdOYm1WNGEyVEIvc0NZaGNveGR4b0ZnK1IwczNHK1BuYnFVSXJuL1dlCmpEci9NRVFyQWdNQkFBRUNnZ0VCQU5BM25NU250WmFhRjhwR25RSE1FbzlITjBzSGFKMUMyWWxUZzZsWVB5WmcKOE9IS0xiYWlNS2x2WU5HY21VMjgxV2VwTEExZUhZVVRoMjQ0VXBWeGkrYzlVbG1zRmVSQnZRemQ0OHFKM1F5bApEcDV3Sk5BYi9PNHdaSERxYVFiUktFSnVvZG1qSTRUSG1lb3FLa2ZBS0xzS25wZXFPWHQ0MmRnSDl5dGxxK3ZkCjFyT2J5cHUyTTFZdXlrcEMzcFhBNlovUlJ6dzU1REZXeUF6UGx0aXora0pydWZkQ08xbzlsZTRQK0xkbW43VEEKZkd3a1RBVlBUYVpCelJFOS9ZaUFaeUhzYUJyZnBXUVFoR1I1Z0tIa09Hbm1YM255dm9JWGFmT05pY2szMkZRNwo2bmJXVEJ1RGdyRnBMYmNzMlNxc1JvY25mRnlwSFNYY1BEdlptZDFTeTJFQ2dZRUE3UFBXR3U4VEp1OTFpaENFCk93WlJXNEE2c0lwRmN4UlJ0WTA0Ri9CTTU1Q0lheEhFc3RxOXl2ZmZHZWhMSVVKVzRtakQrVk1Qd0M5QStseGgKT2NOSDZGbUFCV28xNUVOVmVpR29OL1k1dDRIZTFrTS9Helh0clVSczdKOFFqZTduOEZNcDhocnRRcUxEREIzbgpvblhBbWN4eVpranc5cngybGVLSEtNb3dRZHNDZ1lFQTVjN1N2eWFGZFY2bC9LYm5iWkhVOWVUbDlBdFVVeVdvCmhxT2FvMTh5b2tEaXpnYkZ1ekRXaUxnYllkUzRJSkdjc3k3eUEwWUFQNHI2Q1o3NTFiMjBIcDNILzRCT2ZqWnUKWjlydGlCWitpbFdRMFo4MzB6MU54RENYRHJmS1k2eUxsWFFmbEF4WmhjckJVbHA1NUhaVlk3NGlUbHlyMUVoKwpGa29wSVV1V1gvRUNnWUVBNmlvMmhydUpVOHNGZjRHL0M0Mjh6UTQxOGMxVHdOeHR1MXRwK2M1U1VlMjF3d24yCk4wS1FtWXJJQWhSY1d1dnliU0ZYdW9kcFkyWFBjeHZrUVc5SkdzZUlDdEhobkVrbXFlR2xHbGpNeFJzbEd0MnQKK2JnYndFV0UxM3FDbzZGYnVWYVdkMXBBNnI5cXZnaTNwd2R6WlFwMGE3emQxUmgrb0xVVEdTNW03azBDZ1lFQQp6a0VtblRFeHJ2blgrRDdFajI3SHVEcE11UkJDQ3E1TjV5bUhiUUhRTEJnWWQ5bFVOb0hLeXNLU3NPZCtxcHlHCkM3d2lzaFZ0dTVvOGQ4NGJaVjd4L2xpV1hCY0lXL2IyZUhmaG9MYXZzL0RBSGFQTk11WmVtYXNTcUw4RUF4bWwKM2VsdlBpMG5YQkZ5R1R2akVzMnlMZWRFV3hpSmorblFZS2tHNlQ4eUk4RUNnWUVBN0hoVjgzVlhkOWNDb3A5QQpabTJ6OUFrSkxhdUM2eGIvTkZsM25BcllIREF2UnVndlZsSFdUcm5GditscnRUQzZCYVd3WUF2QVBJZllibm5WCjRTcFNVVFlydGI0RWFYdHFVY0tYeVpqV25KaEhFSGtKYXJFNmxDMmxUNWRCTTdmYmdNclIxbUtFaHRyQ3hmYnoKeG1wYWlJb2tZdEx3dkFEZ2RXWkZhWE1DYjM4PQotLS0tLUVORCBQUklWQVRFIEtFWS0tLS0tCg==",
+		},
 	}
 }
