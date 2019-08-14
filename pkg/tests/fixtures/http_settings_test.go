9b35606a0f64d8933169bb119380e95bb2c9db37
Delyan Raychev <delyan.raychev@microsoft.com>
2019-08-14 14:38:49 -0700

    Adding http_settings_test.go

---
+++ pkg/tests/fixtures/http_settings_test.go
@@ -0,0 +1,34 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+package fixtures
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test Fixtures", func() {
+	Context("Testing GetHTTPSettings1", func() {
+		It("should work as expected", func() {
+			actual := GetHTTPSettings1()
+			Expect(*actual.Name).To(Equal("BackendHTTPSettings-1"))
+		})
+	})
+
+	Context("Testing GetHTTPSettings2", func() {
+		It("should work as expected", func() {
+			actual := GetHTTPSettings2()
+			Expect(*actual.Name).To(Equal("BackendHTTPSettings-2"))
+		})
+	})
+
+	Context("Testing GetHTTPSettings3", func() {
+		It("should work as expected", func() {
+			actual := GetHTTPSettings3()
+			Expect(*actual.Name).To(Equal("BackendHTTPSettings-3"))
+		})
+	})
+})
