4601ff89a274cbea09cdaabf89b97116982388c8
Delyan Raychev <delyan.raychev@microsoft.com>
2019-08-14 14:25:53 -0700

    Adding probes_test.go

---
+++ pkg/tests/fixtures/probes_test.go
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+package fixtures
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Test Fixtures", func() {
+	Context("Testing GetApplicationGatewayProbe", func() {
+		It("should work as expected", func() {
+			host := "host"
+			path := "path"
+			actual := GetApplicationGatewayProbe(&host, &path)
+			expected := "probe-name-aG9zdA-cGF0aA"
+			Expect(*actual.Name).To(Equal(expected))
+		})
+	})
+})
