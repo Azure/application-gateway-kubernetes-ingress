bcb9e6b81466cc65f26033fdfc6740f50f304a3d
Delyan Raychev <delyan.raychev@microsoft.com>
2019-09-12 11:35:42 -0700

    Fixing deliberate linter break

--- pkg/azure/azure.go
+++ pkg/azure/azure.go
@@ -41,7 +41,7 @@ func ConvertToClusterResourceGroup(subscriptionID SubscriptionID, resourceGroup 
 	}
 
 	split := strings.Split(string(resourceGroup), "_")
-	if len(split) != 4 || strings.ToLower(split[0]) != "MC" {
+	if len(split) != 4 || split[0] != "MC" {
 		logLine := fmt.Sprintf("infrastructure resource group name: %s is expected to be of format MC_ResourceGroup_ResourceName_Location", string(resourceGroup))
 		return "", errors.New(logLine)
 	}
