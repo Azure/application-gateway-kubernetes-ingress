419c247c6c57c2bed762aa07857d8dc392067d25
Delyan Raychev <delyan.raychev@microsoft.com>
2019-09-25 12:58:01 -0700

    Adding docstrings

--- cmd/appgw-ingress/errors.go
+++ cmd/appgw-ingress/errors.go
@@ -10,7 +10,12 @@ import (
 )
 
 var (
+	// ErrNoSuchNamespace is an error message.
 	ErrNoSuchNamespace = errors.New("namespace does not exist (MAIN001)")
-	ErrFailedGetToken  = errors.New("failed obtaining auth token (MAIN002)")
-	ErrGetArmAuth      = errors.New("failed arm auth (MAIN003)")
+
+	// ErrFailedGetToken is an error message.
+	ErrFailedGetToken = errors.New("failed obtaining auth token (MAIN002)")
+
+	// ErrGetArmAuth is an error message.
+	ErrGetArmAuth = errors.New("failed arm auth (MAIN003)")
 )
