e9c34a541f980a2772652282be6511a3c86fc7fa
Delyan Raychev <delyan.raychev@microsoft.com>
2019-09-25 13:01:49 -0700

    Addin azure/errors.go

---
+++ pkg/azure/errors.go
@@ -0,0 +1,14 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+package azure
+
+import (
+	"errors"
+)
+
+var (
+	ErrMissingResourceGroup = errors.New("unable to locate AKS resource group (AZUR001)")
+)
