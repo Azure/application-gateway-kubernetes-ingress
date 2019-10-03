e03554fa5876f4ee1a9dc23192fcc12d96141aa3
de <delyan.raychev@microsoft.com>
2019-10-03 12:51:43 -0700

    Lint fixes

--- functional_tests/helpers.go
+++ functional_tests/helpers.go
@@ -11,51 +11,51 @@ import (
 	"io/ioutil"
 	"strings"
 
-	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega"
 
-	. "github.com/Azure/application-gateway-kubernetes-ingress/pkg/appgw"
+	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/appgw"
 	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/environment"
 	"github.com/Azure/application-gateway-kubernetes-ingress/pkg/k8scontext"
 )
 
-func check(cbCtx *ConfigBuilderContext, expected_filename string, stopChan chan struct{}, ctxt *k8scontext.Context, configBuilder ConfigBuilder) {
+func check(cbCtx *appgw.ConfigBuilderContext, expectedFilename string, stopChan chan struct{}, ctxt *k8scontext.Context, configBuilder appgw.ConfigBuilder) {
 	// Start the informers. This will sync the cache with the latest ingress.
 	err := ctxt.Run(stopChan, true, environment.GetFakeEnv())
-	Expect(err).ToNot(HaveOccurred())
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	appGW, err := configBuilder.Build(cbCtx)
-	Expect(err).ToNot(HaveOccurred())
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	jsonBlob, err := appGW.MarshalJSON()
-	Expect(err).ToNot(HaveOccurred())
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	var into map[string]interface{}
 	err = json.Unmarshal(jsonBlob, &into)
-	Expect(err).ToNot(HaveOccurred())
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	jsonBlob, err = json.MarshalIndent(into, "", "    ")
-	Expect(err).ToNot(HaveOccurred())
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	actualJSONTxt := string(jsonBlob)
 
 	// Repair tests
-	// ioutil.WriteFile(expected_filename, []byte(actualJSONTxt), 0644)
+	// ioutil.WriteFile(expectedFilename, []byte(actualJSONTxt), 0644)
 
-	expectedBytes, err := ioutil.ReadFile(expected_filename)
+	expectedBytes, err := ioutil.ReadFile(expectedFilename)
 	expectedJSON := strings.Trim(string(expectedBytes), "\n")
-	Expect(err).ToNot(HaveOccurred())
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	linesAct := strings.Split(actualJSONTxt, "\n")
 	linesExp := strings.Split(expectedJSON, "\n")
 
-	msg := fmt.Sprintf("Line counts are different: %d vs %d\nActual:\n%s\nExpected:\n%s\nfile: %s", len(linesAct), len(linesExp), actualJSONTxt, expectedJSON, expected_filename)
-	Expect(len(linesAct)).To(Equal(len(linesExp)), msg)
+	msg := fmt.Sprintf("Line counts are different: %d vs %d\nActual:\n%s\nExpected:\n%s\nfile: %s", len(linesAct), len(linesExp), actualJSONTxt, expectedJSON, expectedFilename)
+	gomega.Expect(len(linesAct)).To(gomega.Equal(len(linesExp)), msg)
 
 	for idx, line := range linesAct {
 		curatedLineAct := strings.Trim(line, " ")
 		curatedLineExp := strings.Trim(linesExp[idx], " ")
-		msg := fmt.Sprintf("Lines at index %d are different:\n%s\nvs expectedJSON:\n%s\nActual JSON:\n%s\nfrom file %s", idx, curatedLineAct, curatedLineExp, actualJSONTxt, expected_filename)
-		Expect(curatedLineAct).To(Equal(curatedLineExp), msg)
+		msg := fmt.Sprintf("Lines at index %d are different:\n%s\nvs expectedJSON:\n%s\nActual JSON:\n%s\nfrom file %s", idx, curatedLineAct, curatedLineExp, actualJSONTxt, expectedFilename)
+		gomega.Expect(curatedLineAct).To(gomega.Equal(curatedLineExp), msg)
 	}
 
 }
