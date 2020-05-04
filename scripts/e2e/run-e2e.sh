29f5b4ba46a24d1d9a0f6dd31047b2ac6d7f3145
Akshay <aksgupta@microsoft.com>
2020-05-04 22:26:23 +0000

    add a larger timeout. default is 10 m

--- scripts/e2e/run-e2e.sh
+++ scripts/e2e/run-e2e.sh
@@ -36,4 +36,4 @@ InstallAGIC
 
 # run test
 go mod init
-go test -v --tags e2e ./... > testoutput.txt || { echo "go test returned non-zero"; cat testoutput.txt; exit 1; }
+go test -v -timeout 60m -tags e2e ./... > testoutput.txt || { echo "go test returned non-zero"; cat testoutput.txt; exit 1; }
