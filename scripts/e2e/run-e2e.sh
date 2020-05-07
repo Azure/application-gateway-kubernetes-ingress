af36964f335b1c170be3d295a056b11d786408d6
Akshay Gupta <aksgupta@microsoft.com>
2020-05-07 16:01:28 -0700

    e2e: add tests for wildcard hostname (#839)
    * e2e: add tests for wildcard hostname
    
    * add a larger timeout. default is 10 m
    
    * set -ex; move delete helm to separate function

--- scripts/e2e/run-e2e.sh
+++ scripts/e2e/run-e2e.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -ex
 
+function DeleteOtherAGICVersions() {
+    list=$(helm ls --all --short -n agic | grep -v agic-${version})
+    if [[ $list != "" ]]
+    then
+        helm delete $list -n agic
+    fi
+}
+
 function InstallAGIC() {
     [[ -z "${version}" ]] && (echo "version is not set"; exit 1)
     [[ -z "${applicationGatewayId}" ]] && (echo "buiapplicationGatewayIdldid is not set"; exit 1)
     [[ -z "${identityResourceId}" ]] && (echo "identityResourceId is not set"; exit 1)
     [[ -z "${identityClientId}" ]] && (echo "identityClientId is not set"; exit 1)
 
-    echo "Installing BuildId ${version}"
+    DeleteOtherAGICVersions || true
 
-    list=$(helm ls --all --short -n agic | grep -v agic-${version})
-    if [[ $list != "" ]]
-    then
-        helm delete $list -n agic
-    fi
+    echo "Installing BuildId ${version}"
 
     helm repo add staging https://appgwingress.blob.core.windows.net/ingress-azure-helm-package-staging/
     helm repo update
@@ -36,4 +40,4 @@ InstallAGIC
 
 # run test
 go mod init
-go test -v --tags e2e ./... > testoutput.txt || { echo "go test returned non-zero"; cat testoutput.txt; exit 1; }
+go test -v -timeout 60m -tags e2e ./... > testoutput.txt || { echo "go test returned non-zero"; cat testoutput.txt; exit 1; }
