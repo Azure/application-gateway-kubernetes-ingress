6d5475e677883181f2512667e9eefc7ce5329fb7
Oliver Otte <otte@gonicus.de>
2020-09-21 14:52:38 +0200

    Updated regex in health-probes to be RFC conform

--- pkg/appgw/health_probes.go
+++ pkg/appgw/health_probes.go
@@ -164,7 +164,7 @@ func (c *appGwConfigBuilder) generateHealthProbe(backendID backendIdentifier) *n
 	}
 
 	if customPath, err := annotations.BackendProbePath(backendID.Ingress); err == nil {
-		if matched, reErr := regexp.MatchString(`^\/[/.a-zA-Z0-9-]+$`, customPath); matched == true && reErr == nil {
+		if matched, reErr := regexp.MatchString(`^\/([.a-zA-Z0-9-_~\?\#\[\]\@\:\$\&\(\)\*\+\,\;\=]+/??)*$`, customPath); matched == true && reErr == nil {
 			probe.Path = to.StringPtr(customPath)
 			glog.V(5).Infof("Created custom path %s for ingress %s/%s probe", *probe.Path, backendID.Ingress.Namespace, backendID.Ingress.Name)
 		} else {
