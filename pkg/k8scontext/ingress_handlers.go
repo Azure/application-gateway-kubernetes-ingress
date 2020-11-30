4301607784be3448e50345da16befdaa47c93347
Akshay Gupta <aksgupta@microsoft.com>
2020-11-30 14:01:45 -0800

    add log when convertSecret throw error

--- pkg/k8scontext/ingress_handlers.go
+++ pkg/k8scontext/ingress_handlers.go
@@ -115,7 +115,7 @@ func (h handlers) ingressUpdate(oldObj, newObj interface{}) {
 			if secret, exists, err := h.context.Caches.Secret.GetByKey(secKey); exists && err == nil {
 				if !h.context.ingressSecretsMap.ContainsValue(secKey) {
 					if err := h.context.CertificateSecretStore.ConvertSecret(secKey, secret.(*v1.Secret)); err != nil {
-						continue
+						glog.Error(err.Error())
 					}
 				}
 			}
