fa491fcbac5ca3e9d94975c8157c7c6cf608ef85
Akshay Gupta <aksgupta@microsoft.com>
2021-08-02 09:22:15 -0700

    increase to 240

--- scripts/e2e/run-e2e.sh
+++ scripts/e2e/run-e2e.sh
@@ -11,13 +11,13 @@ SetupSharedBackend
 
 # run test
 go mod init || true
-go test -v -timeout 120m -tags e2e ./... >testoutput.txt || true
+go test -v -timeout 240m -tags e2e ./... >testoutput.txt || true
 mv ./cmd/runner/report.xml report.e2e.xml
 
 # install with custom tag
 InstallAGIC "custom-ingress-class"
 
-go test -v -timeout 120m -tags e2eingressclass ./... || true
+go test -v -timeout 240m -tags e2eingressclass ./... || true
 mv ./cmd/runner/report.xml report.e2eingressclass.xml
 
 # print test logs
