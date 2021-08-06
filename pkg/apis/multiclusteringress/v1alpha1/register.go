841a938ec5431b49c931b7439604964810ba5f23
erickhernandezascencio <48027483+erickhernandezascencio@users.noreply.github.com>
2021-08-06 16:20:06 -0700

    (crd) MultiClusterIngress + MultiClusterService CRD base files + codegen (#1244)
    * base files + changes
    
    * new files
    
    * update clusterrole
    
    * Akshay's comments
    
    * go vet fix
    
    Co-authored-by: Akshay Gupta <aksgupta@microsoft.com>

---
+++ pkg/apis/multiclusteringress/v1alpha1/register.go
@@ -0,0 +1,50 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+// +k8s:deepcopy-gen=package,register
+// +groupName=multiclusteringresses.networking.aks.io
+
+// Package v1alpha1 contains API Schema definitions for the MultiClusterIngresses v1alpha1 API group
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{
+		Group:   "networking.aks.io",
+		Version: "v1alpha1",
+	}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+
+	// AddToScheme adds all Resources to the Scheme
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+// Kind takes an unqualified kind and returns back a Group qualified GroupKind
+func Kind(kind string) schema.GroupKind {
+	return SchemeGroupVersion.WithKind(kind).GroupKind()
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// Adds the list of known types to Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&MultiClusterIngress{},
+		&MultiClusterIngressList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
---
+++ pkg/apis/multiclusterservice/v1alpha1/doc.go
@@ -0,0 +1,10 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+// +k8s:deepcopy-gen=package,register
+// +groupName=multiclusterservices.networking.aks.io
+
+// Package v1alpha1 is the v1alpha1 version of the API.
+package v1alpha1
