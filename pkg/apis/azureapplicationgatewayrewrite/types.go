04c7c072247431ad0ae18d8da3a0a22cd5aa677b
paawankohli <44478509+paawankohli@users.noreply.github.com>
2022-05-02 12:28:39 +0000

    AppGw rewrite CRD, CRD example and API code

---
+++ pkg/apis/azureapplicationgatewayrewrite/types.go
@@ -0,0 +1,91 @@
+// -------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// --------------------------------------------------------------------------------------------
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +genclient:nonNamespaced
+
+// AzureApplicationGatewayRewrite is the resource AGIC is watching on for any rewrite rule change
+type AzureApplicationGatewayRewrite struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	// +optional
+	Spec AzureApplicationGatewayRewriteSpec `json:"spec"`
+}
+
+// AzureApplicationGatewayRewriteSpec defines a list of rewrite rules
+type AzureApplicationGatewayRewriteSpec struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// RewriteRules include a list of Application Gateway rewrite rules
+	RewriteRules []RewriteRule `json:"rewriteRules,omitempty"`
+}
+
+// RewriteRule defines a rewrite rule name, rule sequence, a list of conditions and a list of actions
+type RewriteRule struct {
+	Name         string      `json:"name,omitempty"`
+	RuleSequence int         `json:"ruleSequence,omitempty"`
+	Conditions   []Condition `json:"conditions,omitempty"`
+	Actions      []Action    `json:"actions,omitempty"`
+}
+
+// Condition includes IgnoreCase, Negate, Variable and Pattern
+type Condition struct {
+	IgnoreCase bool   `json:"ignoreCase,omitempty"`
+	Negate     bool   `json:"negate,omitempty"`
+	Variable   string `json:"variable,omitempty"`
+	Pattern    string `json:"pattern,omitempty"`
+}
+
+// Action includes RequestHeaderConfigurations, ResponseHeaderConfigurations and UrlConfiguration
+type Action struct {
+	RequestHeaderConfigurations  []RequestHeaderConfiguration  `json:"requestHeaderConfigurations,omitempty"`
+	ResponseHeaderConfigurations []ResponseHeaderConfiguration `json:"responseHeaderConfigurations,omitempty"`
+	UrlConfiguration             UrlConfiguration              `json:"urlConfiguration,omitempty"`
+}
+
+// RequestHeaderConfiguration and ResponseHeaderConfiguration can be same
+
+// RequestHeaderConfiguration includes ActionType, HeaderName and HeaderValue
+type RequestHeaderConfiguration struct {
+	ActionType  string `json:"actionType,omitempty"`
+	HeaderName  string `json:"headerName,omitempty"`
+	HeaderValue string `json:"headerValue,omitempty"`
+}
+
+// ResponseHeaderConfiguration includes ActionType, HeaderName and HeaderValue
+type ResponseHeaderConfiguration struct {
+	ActionType  string `json:"actionType,omitempty"`
+	HeaderName  string `json:"headerName,omitempty"`
+	HeaderValue string `json:"headerValue,omitempty"`
+}
+
+// ResponseHeaderConfiguration includes ModifiedPath, ModifiedQueryString and Reroute
+type UrlConfiguration struct {
+	ModifiedPath        string `json:"modifiedPath,omitempty"`
+	ModifiedQueryString string `json:"modifiedQueryString,omitempty"`
+	Reroute             bool   `json:"reroute,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AzureApplicationGatewayRewriteList is the list of backend pool
+type AzureApplicationGatewayRewriteList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []AzureApplicationGatewayRewrite `json:"items"`
+}
