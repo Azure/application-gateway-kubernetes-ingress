be5d120f4404e7c81dfab6bd18cfa31d84016585
malpaniHMC <malpani.shivam@gmail.com>
2022-08-01 09:35:12 -0700

    wait for gateway only returns an error

--- pkg/azure/client_test.go
+++ pkg/azure/client_test.go
@@ -8,11 +8,13 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-type FakeSender struct{}
+type FakeSender struct{
+	statusCode int
+}
 
 func (fs FakeSender) Do(request *http.Request) (response *http.Response, err error) {
 	response = &http.Response{
-		StatusCode:401,
+		StatusCode:fs.statusCode,
 	}
 	err = errors.New("Error while making a GET for the gateway")
 	return response, err
@@ -24,15 +26,26 @@ func TestClient(t *testing.T) {
 }
 
 var _ = Describe("Azure Client", func() {
-	Context("Az Client Application Gateway Client created successfully", func() {
+	Context("Az Client Application Gateway Client not received successfully (authorizer failure)", func() {
+		It("should handle 400 errors from Azure gracefully", func() {
+
+			var azClient = NewAzClient("", "", "", "", "")
+			var fakeSender = FakeSender{
+				statusCode:400,
+			}
+			azClient.SetSender(fakeSender)
+			err := azClient.WaitForGetAccessOnGateway(10)
+			Expect(err).NotTo(Equal(nil))
+		})
 		It("should handle 401 errors from Azure gracefully", func() {
 
 			var azClient = NewAzClient("", "", "", "", "")
-			var fakeSender = FakeSender{}
+			var fakeSender = FakeSender{
+				statusCode:401,
+			}
 			azClient.SetSender(fakeSender)
-			gateway, err := azClient.GetGateway()
+			err := azClient.WaitForGetAccessOnGateway(10)
 			Expect(err).NotTo(Equal(nil))
-			Expect(gateway).To(Equal(nil))
 		})
 	})
 })
