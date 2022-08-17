ead2429d05ccea482add8398901bdccccc61e8b1
malpaniHMC <ssmalpani@g.hmc.edu>
2022-08-17 16:15:43 -0700

    solving comments and resolving panic

--- pkg/azure/auth_test.go
+++ pkg/azure/auth_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/Azure/go-autorest/autorest/azure/auth"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/klog/v2"
 )
 
 var _ = Describe("Auth Tests", func() {
@@ -19,18 +18,16 @@ var _ = Describe("Auth Tests", func() {
 			os.Unsetenv(auth.ClientSecret)
 		})
 		It("getAuthorizer should try and get some authorizer but fail", func() {
-			authorizer, _ := getAuthorizer("", false, nil)
-			klog.Info(authorizer)
-			//The following tests fail because of the way MSI is setup
-			//Ω(err).To(HaveOccurred())
-			//Ω(authorizer).To(BeNil())
+			authorizer, err := getAuthorizer("", false, nil)
+			Skip("The following tests fail because of the way MSI is setup on Azure Pipelines")
+			Ω(err).To(HaveOccurred())
+			Ω(authorizer).To(BeNil())
 		})
 		It("getAuthorizerWithRetry should try and get some authorizer but fail", func() {
-			authorizer, _ := GetAuthorizerWithRetry("", false, nil, 0, time.Duration(10))
-			klog.Info(authorizer)
-			//The following tests fail because of the way MSI is setup
-			//Ω(err).To(HaveOccurred())
-			//Ω(authorizer).To(BeNil())
+			authorizer, err := GetAuthorizerWithRetry("", false, nil, 0, time.Duration(10))
+			Skip("The following tests fail because of the way MSI is setup on Azure Pipelines")
+			Ω(err).To(HaveOccurred())
+			Ω(authorizer).To(BeNil())
 		})
 	})
 	When("authorizer succeeds", func() {
